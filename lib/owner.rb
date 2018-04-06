d1b8a8e26a41546d324ea5532b0971b4e05b8ff2
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-06 01:40:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,6 +30,15 @@ class Owner
 
   def buy_fish(name)
       @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+      @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+      @pets[:dogs] << Dog.new(name)
+  end
 
 
 
