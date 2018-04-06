2c5b94b2a74fbc076cf83d6c9d9c00c370c383a8
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-06 01:26:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,15 +2,23 @@ class Owner
   # code goes here
   attr_accessor :name
   @@all = []
+  @@count = 0
 
   def initialize(name)
       @name = name
       @@all << self
+      @@count += 1
   end
 
   def self.all
       @all
   end
+
+  def self.count
+
+
+  def self.reset_all
+
 
 
 
